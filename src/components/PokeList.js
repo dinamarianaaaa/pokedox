b5913f96662f15c229a54ffc3c78bad47954e3b1
kiddowin <officiallyanakayah@gmail.com>
2019-09-18 14:48:42 +0700

    added pokemon list

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './styles/PokeList.css';
 import PokeCell from './PokeCell';
+import {pokeClasses} from '../pokeClasses';
 
 const PokeList = () => {
+    const cells = pokeClasses.map(pokeClass => {
+    return (
+        <PokeCell 
+        key={pokeClass.id} 
+        pokeClass={pokeClass} 
+      />
+    );
+  });
+
   return (
     <section className="poke-list">
-      <PokeCell />
-      <PokeCell />
-      <PokeCell />
+    {cells}
     </section>
   )
 }
